a97e4383516006013f1263679ed3ea74783f0ef1
SerhioVed <vedankinsergij@gmail.com>
2026-05-12 18:26:00 +0200

    Login form fixed

--- client/src/modules/auth/index.tsx
+++ client/src/modules/auth/index.tsx
@@ -1,25 +1,2 @@
-import { useTypedSelector } from "../../hooks/useTypedSelector"
-import { CustomLoader } from "../../ui/CustomLoader"
-import { CodeVerifyForm } from "./components/CodeVerifyForm"
-import { LoginForm } from "./components/LoginForm"
-
-export const AuthModule = () => {
-
-    const {isCodeSent, loading} = useTypedSelector(state => state.login)
-
-    if (loading) {
-        return (
-            <CustomLoader/>
-        )
-    }
-
-    if (isCodeSent) {
-        return (
-            <CodeVerifyForm/>
-        )
-    } else {
-        return (
-            <LoginForm/>
-        )
-    }
-}
+export {CodeVerifyForm} from "./components/CodeVerifyForm"
+export {LoginForm} from "./components/LoginForm"
--- client/src/pages/public/loginPage/login.tsx
+++ client/src/pages/public/loginPage/login.tsx
@@ -1,10 +1,26 @@
 import styles from "./login.module.css"
-import { AuthModule } from "../../../modules/auth"
+import { useTypedSelector } from "../../../hooks/useTypedSelector"
+import { CustomLoader } from "../../../ui/CustomLoader"
+import { CodeVerifyForm, LoginForm } from "../../../modules/auth"
 
 export const LoginPage = () => {
+
+    const { isCodeSent, loading } = useTypedSelector(state => state.login)
+
+    if (loading) {
+        return (
+            <CustomLoader />
+        )
+    }
+
     return (
         <div className={styles.container}>
-            <AuthModule/>
+            {isCodeSent
+                ? <CodeVerifyForm/>
+                : <LoginForm/>
+            }
         </div>
     )
+
+
 }
