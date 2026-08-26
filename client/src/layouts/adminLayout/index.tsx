05a20dc4c0a9d52cac1494b019621cb6b56f5513
SerhioVed <vedankinsergij@gmail.com>
2026-08-26 23:35:39 +0200

    Normalize Sidebar component capitalization

--- client/src/layouts/adminLayout/index.tsx
+++ client/src/layouts/adminLayout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom"
 import styles from "./index.module.css"
 import { sideBarLinks } from "../../utils/navigation"
-import { Sidebar } from "../../components/SideBar"
+import { Sidebar } from "../../components/Sidebar"
 import { Header } from "../../components/Header"
 
 export const AdminLayout = () => {
